2771ab586414520a98d8393f71220b84f3f072f4
thung65 <thungtran729@gmail.com>
2026-03-28 11:11:08 +0700

    Update setup7one.sh

--- setup7one.sh
+++ setup7one.sh
@@ -1,4 +1,4 @@
-clear
+Clear
 echo "The owner of these scripts will not be responsible for any unintended problems that may occur whether you edit these scripts or not. It will start in 10 seconds and you have agreed to this. To cancel press Ctrl + C."
 sleep 10
 clear
@@ -11,7 +11,7 @@ fi
 apt update
 apt upgrade -y
 apt install x11-repo -y
-apt install proot-distro p7zip wget pulseaudio -y
+apt install proot-distro p7zip wget pulseaudio curl -y
 clear
 echo -e '\e[1;37mSetting up Pulseaudio...\e[0m'
 export PULSE_SERVER=127.0.0.1
@@ -32,14 +32,14 @@ echo -e '\e[1;37mJust a sec...\e[0m'
 cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root
 curl -o "setup"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setup"$setname"two.sh
 chmod +rwx "setup"$setname".sh"
-echo 'qemu-system-x86_64 -m 2G -smp 4 -cpu core2duo,+avx,+avx2,+sse4.2 -drive file=/storage/emulated/0/VM/W7F.qcow2,if=virtio,aio=threads,cache=unsafe -netdev user,id=n1 -device e1000,netdev=n1 -usb -device usb-tablet -device usb-kbd -device qxl-vga,vgamem_mb=512 -device intel-ac97 -device hda-duplex -accel tcg,thread=multi,tb-size=2048 -vnc :2' > "start"$setname"vm.sh"
+echo 'qemu-system-x86_64 -M q35 -usb -device usb-tablet -device usb-kbd -cpu max,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/W7F.qcow2,aio=threads,cache=unsafe,if=virtio -device virtio-vga,vgamem_mb=128 -device intel-hda -device hda-duplex -netdev user,id=n0 -device rtl8139,netdev=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :2' > "start"$setname"vm.sh"
 chmod +rwx "start"$setname"vm.sh"
 cd ../
 echo "/root/setup"$setname".sh" >> ./etc/profile
 cd
 echo 'sed -i '/start"$setname"/d' /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' > "start"$setname".sh"
 echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> start"$setname".sh
-echo 'echo '/root/start"$setname".sh' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' >> start"$setname".sh
+echo 'echo '/root/start"$setname"vm.sh' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' >> start"$setname".sh
 echo 'proot-distro login debian' >> start"$setname".sh
 chmod +rwx start"$setname".sh
 clear
